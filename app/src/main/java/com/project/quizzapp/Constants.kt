d1fa9603b8ea344b58857565094a8629ee4ba35f
rohanv11 <rohanvaradkar1110@gmail.com>
2020-12-06 18:22:29 +0530

    Added Random variable
    For tracking checkbox on mainactivity

--- app/src/main/java/com/project/quizzapp/Constants.kt
+++ app/src/main/java/com/project/quizzapp/Constants.kt
@@ -18,6 +18,7 @@ object Constants{
     const val USER_NAME : String = "user_name"
     const val TOTAL_QUESTIONS : String = "total_questions"
     const val CORRECT_ANSWERS : String = "correct_answers"
+    const val RANDOM : String = "random"
 
     fun getQuestions():ArrayList<Question>{
         val questionsList = ArrayList<Question>()
